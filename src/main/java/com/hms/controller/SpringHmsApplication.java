f87713a3c2ad30f3940517215bb4deb31861069f
samiralfayed <samiralfayedifti001@gmail.com>
2024-09-15 18:46:07 +0600

    Hospital Management Software

--- src/main/java/com/hms/controller/SpringHmsApplication.java
+++ src/main/java/com/hms/controller/SpringHmsApplication.java
@@ -7,9 +7,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringHmsApplication {
 
 	public static void main(String[] args) {
-		//System.out.println("samir");
-
 		SpringApplication.run(SpringHmsApplication.class, args);
-	}
 
+
+		try {
+			// Load MySQL JDBC driver
+			Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+			System.out.println("Connection established successfully.");
+		} catch (Exception e) {
+				// Handle all exceptions in a single catch block
+				System.out.println("Error loading MySQL JDBC Driver: " + e.toString());
+			}
+
+    }
 }
